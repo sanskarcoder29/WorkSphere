a0e7d50a72d5fe36640f2e4902815cb4cca5427e
Prasiddhi Mishra <bhumimishra91@gmail.com>
2026-07-15 08:06:27 +0530

    fix: resolve Tailwind flexbox alignment on legacy Firefox versions #401 (#472)

--- src/components/EnhancedChatbot.tsx
+++ src/components/EnhancedChatbot.tsx
@@ -966,7 +966,7 @@ export function EnhancedChatbot({
 
   // Render
   return (
-    <div className="flex h-full flex-col bg-white dark:bg-zinc-950 relative overflow-hidden">
+  <div className="flex h-full flex-col min-h-0 bg-white dark:bg-zinc-950 relative overflow-hidden">
       {/* Remote Cursors */}
       <AnimatePresence>
         {Object.values(cursors).map((cursor) => (
